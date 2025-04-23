59ad4add30ecd072f3b97189858ffaa8a256ffe2
yogaekapratama29 <yogaex29@gmail.com>
2025-04-23 12:23:01 +0700

    +Routes Folder and Dynamic Routes

---
+++ src/components/elements/fragments/layouts/Navbar/index.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+    return(
+    <div className="navbar">
+        <div>Navbar</div>
+    </div>
+    )
+}
+export default Navbar;
---
+++ src/pages/setting/app/index.tsx
@@ -0,0 +1,8 @@
+const AppSettingPage = () => {
+    return (
+        <div>
+            <h1>App Setting</h1>
+        </div>
+    )
+}
+export default AppSettingPage;
